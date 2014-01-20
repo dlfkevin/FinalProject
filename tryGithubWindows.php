5fd6110ae5b2e2bbec7fcc91ceb8f8c679730bdf
dlfkevin <dlf.kevin@qq.com>
2014-01-20 16:47:17 +0800

    test
    test

---
+++ tryGithubWindows.php
@@ -0,0 +1,6 @@
+<html>
+	<title>tryGithubWindowsCommit</title>
+	<body>
+		<?php echo "if it can print, that i successed.";?>
+	</body>
+</html>
---
+++ trySecond.php
@@ -0,0 +1,11 @@
+<html>
+	<title>helloworld</title>
+	<body>
+		<?php
+			echo "0";
+			for($i = 1; $i <= 10; $i++){
+				echo "+".$i;
+			}
+		?>
+	</body>
+</html>
